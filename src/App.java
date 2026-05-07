4b5569c297840d721876d5e5e75998095f6dd832
SandroFrontend <djsandro_mdd@hotmail.com>
2026-05-07 19:23:10 +0200

    añado comentario ejecicio 7

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(" Martin Alonso Dominguez");
          System.out.println(" Es majete el chaval");
+         System.out.println("ejercicio 7 ");
     }
 }
